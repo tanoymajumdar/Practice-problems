24e8520b814798b7f7908b19fff1328f015c31c3
tanoymajumdar <49411324+tanoymajumdar@users.noreply.github.com>
2019-04-09 04:01:05 +0800

    Delete statProducts.sh

--- statProducts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in *.log
-do 
-    a=(`./getProductAccess.sh $i | cut -f2 | sort | uniq -c | sort -n -r | head -n 1`)    
-    echo "$i: ${a[1]}"
-    echo "Last three product page access:"
-    grep "/products/" $i | tail -n 3
-    echo
-done    
